2afe5d92cdd26665a09c261c69de45d24921bf11
Kirti Jalan <kirtijalan419@gmail.com>
2023-06-02 14:12:33 -0700

    more exercises

---
+++ exercises/functions/pay.py
@@ -0,0 +1,14 @@
+def payroll(hours, rate):
+    #give the employee 1.5 times the hourly rate for hours worked above 40 hours.
+    if int(hours) > 40:
+        # pay = (int(40) * float(rate)) + ((int(hours)-40)* 1.5 *float(rate))
+        pay = rate * ( int(40) + (hours-40) * 1.5 ) #simplified version of above commented code
+    else: 
+        pay = hours * rate
+    return pay
+
+
+hours = input('Enter Hours:')
+rate = input('Enter Rate:')
+
+print('Pay:', payroll(int(hours), float(rate)))
